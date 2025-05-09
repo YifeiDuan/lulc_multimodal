398e60a3ac3cd08abf347f007e92c23d0351a1be
Yifei Duan <duanyf99@mit.edu>
2025-05-09 11:13:19 -0400

    rand txt

--- geo_txt_gemini.py
+++ geo_txt_gemini.py
@@ -182,3 +182,62 @@ def batch_encode_geo_txt(img_dir="/content/drive/MyDrive/Courses/6.8300/Final Pr
 
 
     
+
+
+def batch_encode_rand_txt(img_dir="/content/drive/MyDrive/Courses/6.8300/Final Project/EuroSAT_MS", 
+                          txt_dim=768,
+                          mode="top", 
+                          samples_per_class=200,
+                          df_path=None):
+    valid_modes = ["all", "selected", "sample", "top"]
+    assert mode in valid_modes, f"choose one valid mode from {valid_modes}"
+
+    # Configure all tif file paths
+    all_tif_files = []
+    if mode == "all":
+        for label in LULC_labels:
+            class_dir = os.path.join(img_dir, label)
+            tif_files = list(Path(class_dir).rglob('*.tif'))        # PosixPath format
+            tif_files = [str(f) for f in tif_files]                 # conver path to str
+            all_tif_files.extend(tif_files)
+    elif mode == "selected":
+        assert isinstance(df_path, str), "provide a str: df_path - that contains selected tif paths"
+        df = pd.read_csv(df_path)
+        for filename in df["Filename"]:
+            tif_path = os.path.join(img_dir, filename)
+            all_tif_files.append(tif_path)
+    elif mode == "sample":
+        assert isinstance(samples_per_class, int), "provide an int: samples_per_class"
+        for label in LULC_labels:
+            class_dir = os.path.join(img_dir, label)
+            tif_files = list(Path(class_dir).rglob('*.tif'))[:samples_per_class]        # PosixPath format
+            tif_files = [str(f) for f in tif_files]                 # conver path to str
+            all_tif_files.extend(tif_files)
+    elif mode == "top":
+        assert isinstance(samples_per_class, int), "provide an int: samples_per_class"
+        for label in LULC_labels:
+            class_dir = os.path.join(img_dir, label)
+            for idx in range(1, samples_per_class+1):
+                tif_file = os.path.join(class_dir, f"{label}_{idx}.tif")
+                all_tif_files.append(tif_file)
+
+    # Prep for saving img embeddings
+    save_pt_name = f"{mode}.pt"
+    if (mode == "sample") or (mode == "top"):
+        save_pt_name = f"{mode}_{samples_per_class}_per_class.pt"
+    save_dir = os.path.join(os.path.dirname(img_dir), "embeddings_rand_txt_gemini")
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    
+    # Get img embeddings for all the tif paths
+    all_embeddings = {}
+    for idx, tif_path in tqdm(enumerate(all_tif_files), total=len(all_tif_files), desc="Encoding for: "):
+        file_id = tif_path.split("/")[-1].split(".")[0]     # "PATH/AnnualCrop_1.tif" -> "AnnualCrop_1"
+
+        embedding = 0.01 * torch.randn(txt_dim)
+        
+        all_embeddings[file_id] = embedding
+
+        if ((idx+1)%200 == 0) or (idx == len(all_tif_files)-1):
+            torch.save(all_embeddings, os.path.join(save_dir, save_pt_name))
+
